019fd16c291d455f222306d7ef75a3d646c5d295
chenlikang <luckbox5566@gmail.com>
2017-08-29 14:46:03 +0800

    Week数据分类并且解析成功

--- src/priv/leon/hzfj/AppliMain.java
+++ src/priv/leon/hzfj/AppliMain.java
@@ -1,12 +1,15 @@
 package priv.leon.hzfj;
 
 
+import priv.leon.hzfj.app.bean.Week;
 import priv.leon.hzfj.app.net.TmsfParasApp;
 import priv.leon.hzfj.net.builder.NetDirector;
 import priv.leon.hzfj.net.builder.WeekPostBuilder;
 import priv.leon.hzfj.net.cookies.WeekCookies;
 import priv.leon.hzfj.net.post.WeekPost;
 import priv.leon.hzfj.net.urls.WeekUrls;
+import priv.leon.hzfj.string.factory.TransFactory;
+import priv.leon.hzfj.string.interfaces.Trans;
 
 import java.util.ArrayList;
 
@@ -15,13 +18,17 @@ public class AppliMain {
         //周报请求参数
         String[] pages= WeekUrls.pages();
         String week_url="http://www.tmsf.com/info/search_infolist.htm";
-        ArrayList<String> week_list=new ArrayList<>();
+        ArrayList<String> week_s_list=new ArrayList<>();
 
-        //获取Cookies
          NetDirector director=new NetDirector(pages,week_url);
          WeekPostBuilder week_post=new WeekPostBuilder();
          director.construct(week_post);
-         week_list=week_post.getResult();
-         System.out.print(week_list.get(0));
+         week_s_list=week_post.getResult();
+
+
+        Trans trans=TransFactory.get("周报");
+        ArrayList<Week> week_b_list=new ArrayList<>();
+        week_b_list=trans.trans(week_s_list);
+        System.out.print(week_s_list.get(0));
     }
 }
